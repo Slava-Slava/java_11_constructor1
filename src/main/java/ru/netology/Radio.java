839b622e72fcc4455af8db7764cfe8b2b729440a
Slava Vasiliev <sb-fantom@yandex.ru>
2023-04-06 21:38:14 +0300

    change3-2 README.md

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -14,4 +14,43 @@ public class Radio {
     private int currentVolume;
     private int maxVolume = 100;
     private int minVolume = 0;
+
+    public Radio(int quantity) {
+        this.currentStationNumber = quantity - 1;
+    }
+
+    public void pressNextStation() {
+        if (currentStationNumber >= maxRadioStation) {
+            setCurrentStationNumber(minRadioStation);
+        } else {
+            setCurrentStationNumber(currentStationNumber + 1);
+        }
+    }
+
+    public void pressPrevStation() {
+        if (currentStationNumber <= minRadioStation) {
+            setCurrentStationNumber(maxRadioStation);
+        } else {
+            setCurrentStationNumber(currentStationNumber - 1);
+        }
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume() {
+        this.minVolume = minVolume;
+    }
+
+    public void pressPlusVolume() {
+
+        setCurrentVolume(currentVolume + 1);
+    }
+
+    public void pressMinusVolume() {
+
+        setCurrentVolume(currentVolume - 1);
+    }
+
 }
